ed98773282bf504d1f43c0fe8c9ad07c734c3976
CodeMachine0121 <CodeMachine0121@gmail.com>
2024-04-28 01:50:19 +0800

    feat: change colorscheme

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,26 +1,16 @@
 return {
-
-    {
-        "akinsho/bufferline.nvim",
-        keys = {
-            { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
-            { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Previous tab" },
-        },
-        opts = {
-            options = {
-                mode = "tabs",
-                show_buffer_close_icons = false,
-                show_close_icon = false,
-            },
-        },
-    },
-    {
-        "nvim-lualine/lualine.nvim",
-        event = "VeryLazy",
-        opts = {
-            options = {
-                themes = "solarized_dark",
-            },
-        },
-    },
+	{
+		"akinsho/bufferline.nvim",
+		keys = {
+			{ "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+			{ "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Previous tab" },
+		},
+		opts = {
+			options = {
+				mode = "tabs",
+				show_buffer_close_icons = false,
+				show_close_icon = false,
+			},
+		},
+	},
 }
